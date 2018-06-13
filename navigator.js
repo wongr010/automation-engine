7f543901434ff7eb2fbd8a8480d4324f9190a280
Wong <rosalyn.wong@accenture.com>
2018-06-13 13:56:29 -0400

    finding matching bubbles using react selector

--- navigator.js
+++ navigator.js
@@ -1,14 +1,6 @@
 import {Selector} from 'testcafe';
+import {ReactSelector} from 'testcafe-react-selectors';
 
-export async function numClicksForButton(label){
-	var numClicks=0;
-	try{
-		numclicks=await actionbarNavigator(label);
-	} catch (error){
-		console.trace();
-	}
-	return numClicks;
-}
 
 export async function actionbarNavigator(label){
 	var numMatching= await Selector('.button').count; //Selector is a promise, await makes it return number type
@@ -23,4 +15,24 @@ export async function actionbarNavigator(label){
 	}
 
 	return i+1; //starting from 1 click
+}
+
+export async function findMatchingBubbleNo(text){
+	const count_groups=await ReactSelector('t div div').find('.chat-window__content').find('.chat-bubble__bubble-proper').count;
+	const bubbles=ReactSelector('t div div').find('.chat-window__content').find('.chat-bubble__bubble-proper').nth(count_groups-1);
+	const widget_count=await bubbles.find('div').findReact('t').withProps({name: "text"}).count;
+
+	for (var i=0; i<widget_count; i++){
+		const widget=bubbles.find('div').findReact('t').nth(i);
+		const props=await widget.getReact();
+		const extract=JSON.stringify(props, null, 2);
+		const parsed=JSON.parse(extract);
+		console.log(JSON.stringify(parsed['props']['widgetDSL']['data']['text']));
+		if (JSON.stringify(parsed['props']['widgetDSL']['data']['text'])==='"'+text+'"'){ //a bit too hard coded rn
+			console.log(i);
+			return i;
+		}
+	}
+
+	return i;
 }
